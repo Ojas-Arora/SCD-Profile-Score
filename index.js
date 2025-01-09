4ddd54caa7f377e968701d30275794dc04e380ce
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-10 00:09:13 +0530

    Fixed Add Animation in About us page card

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ tl.from(".nav-links li", {
     duration: 1,
     y: -30
 })
+
+// About us Page 
+gsap.from(".feature-card", {
+    scale: 0,
+    duration: 1,
+    rotate: 1440,
+    ease: "steps.out"
+})
